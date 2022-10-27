ad01b164991143aed3fd58b02729ddd2696d30ea
Maria Jose Caro <maria.jose.caro.esp@gmail.com>
2022-10-27 00:04:43 -0300

    2

--- src/main/java/org/soyphea/k8s/controller/Controller.java
+++ src/main/java/org/soyphea/k8s/controller/Controller.java
@@ -10,6 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.security.Key;
+import java.security.Security;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import java.util.List;
 
@@ -29,7 +38,10 @@ public class Controller {
     @GetMapping("/k8s/{name}")
     public String k8sGreeting(@PathVariable("name") String name) {
 
-        
+        byte[] salt = "notrandom".getBytes();
+
+        PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Noncompliant, predictable salt
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, 10000, 256); // Noncompliant, predictable salt
         log.info("Got the request with name:{}", name);
         return String.format("Hi %s- I am ConfigMap running in side k8s with value %s", name,userConfig);
     }
