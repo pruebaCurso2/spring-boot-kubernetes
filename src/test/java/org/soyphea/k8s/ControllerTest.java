e144d29e43c83c569dce1bab5583086306e93995
Coty-c <104175053+Coty-c@users.noreply.github.com>
2022-10-26 22:50:33 -0300

    Update ControllerTest.java

--- src/test/java/org/soyphea/k8s/ControllerTest.java
+++ src/test/java/org/soyphea/k8s/ControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import java.sql.Connection;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
