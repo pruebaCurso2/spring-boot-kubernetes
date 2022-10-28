ec4203f5a1908cf6079552a413ebadd856ef4d96
Coty-c <104175053+Coty-c@users.noreply.github.com>
2022-10-27 21:31:37 -0300

    Create coty.java

---
+++ src/main/java/org/soyphea/k8s/coty.java
@@ -0,0 +1,29 @@
+package org.soyphea.k8s;
+
+import lombok.extern.slf4j.Slf4j;
+import org.soyphea.k8s.config.UserConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class K8SApplication implements CommandLineRunner {
+
+
+    @Autowired
+    UserConfig userConfig;
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8SApplication.class, args);
+        
+        
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(" Fetched user from k8s ConfigMap <spring-boot-k8s> is :{}", userConfig);
+
+    }
+}
